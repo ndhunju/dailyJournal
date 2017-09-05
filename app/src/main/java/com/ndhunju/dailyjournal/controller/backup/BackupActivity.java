f3b0733dbc59140b272819c93372c7aa7f5139f9
ndhunju <nikesh_dhunju@hotmail.com>
2017-09-04 18:35:22 -0700

    Since BackupActivity is a second level activity, removed it as a subclass of NavDrawerActivity. User will now have to click on Up button to go to previous activity.

--- app/src/main/java/com/ndhunju/dailyjournal/controller/backup/BackupActivity.java
+++ app/src/main/java/com/ndhunju/dailyjournal/controller/backup/BackupActivity.java
@@ -3,23 +3,40 @@ package com.ndhunju.dailyjournal.controller.backup;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.ndhunju.dailyjournal.R;
-import com.ndhunju.dailyjournal.controller.NavDrawerActivity;
 
 /** Created by Dhunju on 8/14/2016.
  * This activity groups together tools relevant to backing up. */
-public class BackupActivity extends NavDrawerActivity {
+public class BackupActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_backup);
+
+        setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getFragmentManager().beginTransaction().add(R.id.content_frame,
                 new BackupPreferenceFragment(), BackupPreferenceFragment.TAG)
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                 .commit();
 
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
