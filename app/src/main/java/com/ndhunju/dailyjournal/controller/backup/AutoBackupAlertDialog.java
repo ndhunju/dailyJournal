497b3de8d91ce27d627ada9ddf64016943fdf9f6
ndhunju <nikesh_dhunju@hotmail.com>
2023-12-27 19:12:38 -0500

    Improve auto backup restore dialog.

--- app/src/main/java/com/ndhunju/dailyjournal/controller/backup/AutoBackupAlertDialog.java
+++ app/src/main/java/com/ndhunju/dailyjournal/controller/backup/AutoBackupAlertDialog.java
@@ -2,7 +2,6 @@ package com.ndhunju.dailyjournal.controller.backup;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 
 import com.ndhunju.dailyjournal.R;
 import com.ndhunju.dailyjournal.util.UtilsFile;
@@ -37,38 +36,56 @@ public class AutoBackupAlertDialog {
         AlertDialog.Builder builder= new AlertDialog.Builder(mActivity)
                 .setTitle(mActivity.getString(R.string.str_choose))
                 .setNegativeButton(android.R.string.cancel, null)
-                .setMultiChoiceItems(filesNames, null, new DialogInterface.OnMultiChoiceClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i, boolean b) {
-                        selectedItems[i] = b;
-                    }
-                })
-                .setNegativeButton(R.string.str_delete, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //delete all selected items
-                        for (int j = 0; j < backupFiles.length; j++) {
-                            if (selectedItems[j]) {
-                                UtilsFile.deleteFile(backupFiles[j].getAbsolutePath());
-                            }
-
-                            UtilsView.toast(mActivity, mActivity
-                                    .getString(R.string.msg_deleted, mActivity.getString(R.string.str_backup)));
-                        }
-                    }
-                })
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //restore from first selected file
-                        for (int j = 0; j < backupFiles.length; j++) {
-                            if (selectedItems[j]) {
-                                new RestoreBackUpAsync(mActivity).execute(backupFiles[j].getAbsolutePath());
-                                return;
-                            }
+                .setMultiChoiceItems(
+                        filesNames,
+                        null,
+                        (dialogInterface, i, b) -> selectedItems[i] = b
+                )
+                .setNegativeButton(R.string.str_delete, (dialogInterface, i) -> {
+                    // Delete all selected items
+                    for (int j = 0; j < backupFiles.length; j++) {
+                        if (selectedItems[j]) {
+                            UtilsFile.deleteFile(backupFiles[j].getAbsolutePath());
                         }
 
+                        UtilsView.toast(mActivity, mActivity
+                                .getString(
+                                        R.string.msg_deleted,
+                                        mActivity.getString(R.string.str_backup))
+                        );
                     }
+                })
+                .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> {
+
+                    int selectedItemCount = 0;
+                    int firstSelectedItemIndex = -1;
+
+                    for (int j = 0; j < backupFiles.length; j++) {
+                        if (selectedItems[j]) {
+                            selectedItemCount++;
+                            if (firstSelectedItemIndex == -1) {
+                                firstSelectedItemIndex = j;
+                            }
+                        }
+                    }
+
+                    // Show error message if user selects 0 or 1+ items
+                    if (selectedItemCount > 1 || selectedItemCount < 1) {
+                        UtilsView.alert(
+                                AutoBackupAlertDialog.this.mActivity,
+                                mActivity.getString(R.string.error_msg_invalid_backup_file_count_selected)
+                        );
+                        return;
+                    }
+
+                    // Backup the first selected item. There should be only one at this point
+                    if (selectedItems[firstSelectedItemIndex]) {
+                        new RestoreBackUpAsync(mActivity).execute(
+                                backupFiles[firstSelectedItemIndex].getAbsolutePath()
+                        );
+                        return;
+                    }
+
                 });
 
      return   builder.create();
